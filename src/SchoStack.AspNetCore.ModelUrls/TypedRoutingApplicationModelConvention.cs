375fa4970a115c9059bdf643e85369bd846f19b8
Gabriel Sadaka <gabriel.sadaka@inlogik.com>
2018-02-05 16:59:02 +1100

    add typed route conventions to DI

--- src/SchoStack.AspNetCore.ModelUrls/TypedRoutingApplicationModelConvention.cs
+++ src/SchoStack.AspNetCore.ModelUrls/TypedRoutingApplicationModelConvention.cs
@@ -1,11 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace SchoStack.AspNetCore.ModelUrls
 {
     public class TypedRoutingApplicationModelConvention : IApplicationModelConvention
     {
+        public Dictionary<Type, RouteInformation> RouteInformations { get; }
+
+        public TypedRoutingApplicationModelConvention(Dictionary<Type, RouteInformation> routeInformations)
+        {
+            RouteInformations = routeInformations;
+        }
+
         public void Apply(ApplicationModel application)
         {
             foreach (var controller in application.Controllers)
@@ -17,9 +28,20 @@ namespace SchoStack.AspNetCore.ModelUrls
                     if (route != null && oneParam != null && oneParam.ParameterInfo.ParameterType.GetTypeInfo().IsClass && oneParam.ParameterInfo.ParameterType != typeof(string))
                     {
                         route.Name = oneParam.ParameterInfo.ParameterType.ToString();
+                        RouteInformations[oneParam.ParameterInfo.ParameterType] = new RouteInformation
+                        {
+                            Method = controllerAction.Attributes.OfType<HttpPostAttribute>().Any() ? HttpMethod.Post : HttpMethod.Get,
+                            ActionName = controllerAction.ActionName
+                        };
                     }
                 }
             }
         }
+    }
+
+    public class RouteInformation
+    {
+        public HttpMethod Method { get; set; }
+        public string ActionName { get; set; }
     }
 }
