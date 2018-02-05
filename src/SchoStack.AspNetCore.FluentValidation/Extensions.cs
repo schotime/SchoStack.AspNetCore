a5403ba54d2ae217436259965f70d18dae59a16f
Gabriel Sadaka <gabriel.sadaka@inlogik.com>
2018-02-05 16:49:31 +1100

    add validator finder and fluent validation options to DI

--- src/SchoStack.AspNetCore.FluentValidation/Extensions.cs
+++ src/SchoStack.AspNetCore.FluentValidation/Extensions.cs
@@ -1,13 +1,31 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation;
+using HtmlTags;
 using Microsoft.Extensions.DependencyInjection;
+using SchoStack.AspNetCore.HtmlConventions.Core;
 
 namespace SchoStack.AspNetCore.FluentValidation
 {
     public static class Extensions
     {
-        public static void AddFluentValidationHtmlConventions(this IServiceCollection serviceCollection)
+        public static void AddFluentValidationHtmlConventions(this IServiceCollection serviceCollection, Action<FluentValidationOptions> options = null)
         {
-            serviceCollection.AddSingleton(x => new FluentValidatorFinder(y => (IValidator) x.GetService(y)));
+            var fluentValidationOptions = new FluentValidationOptions();
+            options?.Invoke(fluentValidationOptions);
+            
+            serviceCollection.AddSingleton<IValidatorFinder>(x => new FluentValidatorFinder(y => (IValidator) x.GetService(y)));
+            serviceCollection.AddSingleton(new ServiceDescriptor(typeof(FluentValidationOptions), fluentValidationOptions));
+        }
+    }
+
+    public class FluentValidationOptions
+    {
+        public List<Action<IEnumerable<PropertyValidatorResult>, HtmlTag, RequestData>> RuleProviders = new List<Action<IEnumerable<PropertyValidatorResult>, HtmlTag, RequestData>>();
+
+        public void AddRuleProviders(Action<IEnumerable<PropertyValidatorResult>, HtmlTag, RequestData> ruleProvider)
+        {
+            RuleProviders.Add(ruleProvider);
         }
     }
 }
